fd34d9c2032036eba0fa4052cb53c6c537044027
jalvarez <esi.segurity@gmail.com>
2023-11-28 06:19:35 -0500

    Actualizacion de metodos
    se actualizo el nombre de los metodos

--- orm/__init__.py
+++ orm/__init__.py
@@ -6,7 +6,7 @@ class SQLiteORM:
         self.conn = sqlite3.connect(f"{db_name}.db")
         self.cursor = self.conn.cursor()
 
-    def create_table(self, table_definition):
+    def crear_tabla(self, table_definition):
         # Crea una tabla a partir de la definición en formato JSON
         for table_name, columns in table_definition.items():
             columns_str = ", ".join([f"{col_name} {col_type}" for col_name, col_type in columns.items()])
@@ -14,7 +14,7 @@ class SQLiteORM:
             self.cursor.execute(query)
         self.conn.commit()
 
-    def insert(self, table_name, data):
+    def insertar(self, table_name, data):
         # Inserta un nuevo registro en la tabla
         columns = ', '.join(data.keys())
         values = ', '.join([f":{key}" for key in data.keys()])
@@ -22,20 +22,20 @@ class SQLiteORM:
         self.cursor.execute(query, data)
         self.conn.commit()
 
-    def update(self, table_name, data, where):
+    def actualizar(self, table_name, data, where):
         # Actualiza registros en la tabla
         set_clause = ', '.join([f"{key} = :{key}" for key in data.keys()])
         query = f"UPDATE {table_name} SET {set_clause} WHERE {where}"
         self.cursor.execute(query, data)
         self.conn.commit()
 
-    def delete(self, table_name, where):
+    def eliminar(self, table_name, where):
         # Elimina registros de la tabla
         query = f"DELETE FROM {table_name} WHERE {where}"
         self.cursor.execute(query)
         self.conn.commit()
 
-    def select(self, table_name, where=None):
+    def mostrar(self, table_name, where=None):
         # Realiza una consulta SELECT
         query = f"SELECT * FROM {table_name}"
         if where:
@@ -52,5 +52,5 @@ class SQLiteORM:
 
         return results
 
-    def close(self):
+    def cerrar(self):
         self.conn.close()
